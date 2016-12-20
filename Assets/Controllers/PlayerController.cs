90fa84dbb7fd85b7d293b74e8815923c5e98d01d
YashPeter <yashp17@g.eips.ca>
2016-12-19 19:26:05 -0700

    Dec19
    Home Fixed our Problems

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -5,196 +5,199 @@ using System;
 //controls the visuals of the player only//
 public class PlayerController : MonoBehaviour
 {
-	//Importing Sprites into the main class
-	public Sprite tester;
-	public Sprite p1l1;
-	public Sprite p1l2;
-	public Sprite p1l3;
-	public Sprite p1l4;
-	public Sprite p1l5;
+    //Importing Sprites into the main class
+    public Sprite p1l1;
+    public Sprite p1l2;
+    public Sprite p1l3;
+    public Sprite p1l4;
+    public Sprite p1l5;
 
-	public Sprite p1r1;
-	public Sprite p1r2;
-	public Sprite p1r3;
-	public Sprite p1r4;
-	public Sprite p1r5;
+    public Sprite p1r1;
+    public Sprite p1r2;
+    public Sprite p1r3;
+    public Sprite p1r4;
+    public Sprite p1r5;
 
-	public Sprite p1u1;
-	public Sprite p1u2;
-	public Sprite p1u3;
-	public Sprite p1u4;
-	public Sprite p1u5;
+    public Sprite p1u1;
+    public Sprite p1u2;
+    public Sprite p1u3;
+    public Sprite p1u4;
+    public Sprite p1u5;
 
-	public Sprite p1d1;
-	public Sprite p1d2;
-	public Sprite p1d3;
-	public Sprite p1d4;
-	public Sprite p1d5;
-	// Use this for initialization
+    public Sprite p1d1;
+    public Sprite p1d2;
+    public Sprite p1d3;
+    public Sprite p1d4;
+    public Sprite p1d5;
+    // Use this for initialization
 
-	public static PlayerController Instance { get; protected set; }
+    public static PlayerController Instance { get; protected set; }
 
-	public float playerSpeed = 12;
+    public float playerSpeed = 12;
 
-	public Action<int, int, int, int> upf;
-	public Action<int, float, float, int> upp;
-	GameObject[] playerdisplay;
-	SpriteRenderer[] playersprite;
-	Rigidbody2D[] playermovement;
+    public Action<int, int, int, int> upf;
+    public Action<int, float, float, int> upp;
+    GameObject[] playerdisplay;
+    SpriteRenderer[] playersprite;
+    Rigidbody2D[] playermovement;
 
-	void Start()
-	{   Debug.Log ("playercontroller started");
-		playerdisplay = new GameObject[6];
-		playersprite = new SpriteRenderer[6];
-		Instance = this;
-		upf = changeGivenPlayerFrame;
-		upp = translateGivenPlayerFrame;
-		//return WorldController.Instance.World.*insertmethod*
-	}
+    void Start()
+    {
+        Instance = this;
+        Debug.Log("playercontroller started");
+        playerdisplay = new GameObject[6];
+        playersprite = new SpriteRenderer[6];
+        upf = changeGivenPlayerFrame;
+        upp = translateGivenPlayerFrame;
+        //return WorldController.Instance.World.*insertmethod*
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+    }
 
-	public Action<int, int, int, int> returnUPF()
-	{
-		return upf;
-	}
+    public Action<int, int, int, int> returnUPF()
+    {
+        Debug.Log("testing");
+        return upf;
+    }
 
-	public Action<int, float, float, int> returnUPP()
-	{
-		return upp;
-	}
+    public Action<int, float, float, int> returnUPP()
+    {
+        return upp;
+    }
 
-	public void changeGivenPlayerFrame(int arraypos, int frame, int direction, int currplay)
-	{
+    public void changeGivenPlayerFrame(int arraypos, int frame, int direction, int currplay)
+    {
 
-		if (playerdisplay [arraypos] == null) {
-			playerdisplay [arraypos] = new GameObject ();
-			playerdisplay [arraypos].AddComponent<SpriteRenderer> ();
-			playersprite [arraypos] = new SpriteRenderer ();
-			playersprite [arraypos] = playerdisplay [arraypos].GetComponent<SpriteRenderer> ();
-			playerdisplay[arraypos].AddComponent<Rigidbody2D>();
-			/**playermovement[arraypos] = playerdisplay [arraypos].GetComponent<Rigidbody2D>();
-             playermovement[arraypos] = playerdisplay[arraypos].GetComponent<Rigidbody2D>();
-      
-            playermovement[arraypos].gravityScale = 0;
-              **/
+        if (playerdisplay[arraypos] == null)
+        {
+            playerdisplay[arraypos] = new GameObject();
+            playerdisplay[arraypos].AddComponent<SpriteRenderer>();
+            playersprite[arraypos] = new SpriteRenderer();
+            playersprite[arraypos] = playerdisplay[arraypos].GetComponent<SpriteRenderer>();
+           
+            
+        }
 
-		}
+        switch (frame)
+        {
+            case 1:
+                switch (direction)
+                {
+                    case 0:
+                        playersprite[arraypos].sprite = p1d1;
+                        break;
 
-		switch (frame) {
-		case 1:
-			switch (direction) {
-			case 0:
-				playersprite [arraypos].sprite = p1d1;
-				break;
+                    case 1:
+                        playersprite[arraypos].sprite = p1r1;
+                        break;
 
-			case 1:
-				playersprite [arraypos].sprite = p1r1;
-				break;
+                    case 2:
+                        playersprite[arraypos].sprite = p1u1;
+                        break;
 
-			case 2:
-				playersprite [arraypos].sprite = p1u1;
-				break;
+                    case 3:
+                        playersprite[arraypos].sprite = p1l1;
+                        break;
 
-			case 3:
-				playersprite [arraypos].sprite = p1l1;
-				break;
+                }
 
-			}
+                break;
 
-			break;
+            case 2:
+                switch (direction)
+                {
+                    case 0:
+                        playersprite[arraypos].sprite = p1d2;
+                        break;
 
-		case 2:
-			switch (direction) {
-			case 0:
-				playersprite [arraypos].sprite = p1d2;
-				break;
+                    case 1:
+                        playersprite[arraypos].sprite = p1r2;
+                        break;
 
-			case 1:
-				playersprite [arraypos].sprite = p1r2;
-				break;
+                    case 2:
+                        playersprite[arraypos].sprite = p1u2;
+                        break;
 
-			case 2:
-				playersprite [arraypos].sprite = p1u2;
-				break;
+                    case 3:
+                        playersprite[arraypos].sprite = p1l2;
+                        break;
 
-			case 3:
-				playersprite [arraypos].sprite = p1l2;
-				break;
+                }
+                break;
 
-			}
-			break;
+            case 3:
+                switch (direction)
+                {
+                    case 0:
+                        playersprite[arraypos].sprite = p1d3;
+                        break;
 
-		case 3:
-			switch (direction) {
-			case 0:
-				playersprite [arraypos].sprite = p1d3;
-				break;
+                    case 1:
+                        playersprite[arraypos].sprite = p1r3;
+                        break;
 
-			case 1:
-				playersprite [arraypos].sprite = p1r3;
-				break;
+                    case 2:
+                        playersprite[arraypos].sprite = p1u3;
+                        break;
 
-			case 2:
-				playersprite [arraypos].sprite = p1u3;
-				break;
+                    case 3:
+                        playersprite[arraypos].sprite = p1l3;
+                        break;
 
-			case 3:
-				playersprite [arraypos].sprite = p1l3;
-				break;
+                }
+                break;
 
-			}
-			break;
+            case 4:
+                switch (direction)
+                {
+                    case 0:
+                        playersprite[arraypos].sprite = p1d4;
+                        break;
 
-		case 4:
-			switch (direction) {
-			case 0:
-				playersprite [arraypos].sprite = p1d4;
-				break;
+                    case 1:
+                        playersprite[arraypos].sprite = p1r4;
+                        break;
 
-			case 1:
-				playersprite [arraypos].sprite = p1r4;
-				break;
+                    case 2:
+                        playersprite[arraypos].sprite = p1u4;
+                        break;
 
-			case 2:
-				playersprite [arraypos].sprite = p1u4;
-				break;
+                    case 3:
+                        playersprite[arraypos].sprite = p1l4;
+                        break;
 
-			case 3:
-				playersprite [arraypos].sprite = p1l4;
-				break;
+                }
+                break;
 
-			}
-			break;
+            case 5:
+                switch (direction)
+                {
+                    case 0:
+                        playersprite[arraypos].sprite = p1d5;
+                        break;
 
-		case 5:
-			switch (direction) {
-			case 0:
-				playersprite [arraypos].sprite = p1d5;
-				break;
+                    case 1:
+                        playersprite[arraypos].sprite = p1r5;
+                        break;
 
-			case 1:
-				playersprite [arraypos].sprite = p1r5;
-				break;
+                    case 2:
+                        playersprite[arraypos].sprite = p1u5;
+                        break;
 
-			case 2:
-				playersprite [arraypos].sprite = p1u5;
-				break;
+                    case 3:
+                        playersprite[arraypos].sprite = p1l5;
+                        break;
 
-			case 3:
-				playersprite [arraypos].sprite = p1l5;
-				break;
+                }
+                break;
+        }
+    }
 
-			}
-			break;
-		}
-	}
-
-	public void translateGivenPlayerFrame(int arraypos, float x, float y, int direction)
+    public void translateGivenPlayerFrame(int arraypos, float x, float y, int direction)
 	{
 		
 
--- Assets/Controllers/WorldController.cs
+++ Assets/Controllers/WorldController.cs
@@ -2,81 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-public class WorldController : MonoBehaviour {
+public class WorldController : MonoBehaviour
+{
 
-	//allows other controllers to access this class, handy//
-	public static WorldController Instance { get; protected set; }
+    //allows other controllers to access this class, handy//
+    public static WorldController Instance { get; protected set; }
 
-	//sets up an instance so the playerdisplay can access this/
-	public WorldData World { get; protected set; }
+    //sets up an instance so the playerdisplay can access this/
+    public WorldData World { get; protected set; }
 
-	//action iniciated//
-	Action <int, int, int, int> upf;
-	Action<int, float, float, int> upp;
-
-
-	void Start () {
+    //action iniciated//
+    Action<int, int, int, int> upf;
+    Action<int, float, float, int> upp;
 
 
-		//sets up actions from playercontroller to be sent to player objects using player controller instance//
-		upf = PlayerController.Instance.returnUPF();
+    void Start()
+    {
 
-		upp = PlayerController.Instance.returnUPP();
+        upf = PlayerController.Instance.returnUPF();
+        upp = PlayerController.Instance.returnUPP();
+        Instance = this;
+        World = new WorldData(upf, upp);
+        //sets up actions from playercontroller to be sent to player objects using player controller instance//
+      
 
-		//sets instance to this specific version//
-		Instance = this;
-		//sets up world data//
-		World = new WorldData(upf, upp);
+        //sets instance to this specific version//
 
-		//add a player, just for testing//
-		World.AddPlayer();
+        //sets up world data//
 
-	}
 
-	// Update is called once per frame
-	void Update () {
+        //add a player, just for testing//
+        World.AddPlayer();
 
-		//work in progress//
-		if (Input.GetKeyUp("s")) {
-			if (World.ReturnPlayerDirection(0) != 0 )
-			{
-				World.SetPlayerDirection(0, 0);
-			}
-			else
-			{
-				World.MovePlayer(0);
-			}
-		}
-		if (Input.GetKeyUp("d")) {
-			if (World.ReturnPlayerDirection(0) != 1)
-			{
-				World.SetPlayerDirection(0, 1);
-			}
-			else
-			{
-				World.MovePlayer(0);
-			}
-		}
-		if (Input.GetKeyUp("w")) {
-			if (World.ReturnPlayerDirection(0) != 2)
-			{
-				World.SetPlayerDirection(0, 2);
-			}
-			else
-			{
-				World.MovePlayer(0);
-			}
-		}
-		if (Input.GetKeyUp("a")) {
-			if (World.ReturnPlayerDirection(0) != 3)
-			{
-				World.SetPlayerDirection(0, 3);
-			}
-			else
-			{
-				World.MovePlayer(0);
-			}
-		}
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        //work in progress//
+        if (Input.GetKeyUp("s"))
+        {
+            if (World.ReturnPlayerDirection(0) != 0)
+            {
+                World.SetPlayerDirection(0, 0);
+            }
+            else
+            {
+                World.MovePlayer(0);
+            }
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            if (World.ReturnPlayerDirection(0) != 1)
+            {
+                World.SetPlayerDirection(0, 1);
+            }
+            else
+            {
+                World.MovePlayer(0);
+            }
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            if (World.ReturnPlayerDirection(0) != 2)
+            {
+                World.SetPlayerDirection(0, 2);
+            }
+            else
+            {
+                World.MovePlayer(0);
+            }
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            if (World.ReturnPlayerDirection(0) != 3)
+            {
+                World.SetPlayerDirection(0, 3);
+            }
+            else
+            {
+                World.MovePlayer(0);
+            }
+        }
+
+    }
+
+    
 }
